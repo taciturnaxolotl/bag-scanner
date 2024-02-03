aaf247aa4d6cd215c3b2b8e9b202a04793223b88
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-02-03 01:09:57 -0500

    feat: add csv loading
    Signed-off-by: Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>

--- index.ts
+++ index.ts
@@ -1,5 +1,38 @@
 import puppeteer from 'puppeteer';
+import fs from 'fs';
+import path from 'path';
+import { parse } from 'csv-parse'
 import { loginToSlack } from './utils/login-to-slack';
+
+type hackclubers = {
+  name: string;
+  whatIDo: string;
+  accountType: string;
+  accountCreated: string;
+  daysActive: string;
+  messagesPosted: string;
+};
+
+const hackclubers: hackclubers[] = [];
+(() => {
+  const csvFilePath = path.resolve(import.meta.dir, 'hackclub-users.csv');
+
+  const headers = ['name', 'whatIDo', 'accountType', 'accountCreated', 'daysActive', 'messagesPosted'];
+
+  const fileContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' });
+
+  parse(fileContent, {
+    delimiter: ',',
+    columns: headers,
+  }, (error, result: hackclubers[]) => {
+    if (error) {
+      console.error(error);
+    }
+    console.log("Result", result);
+    hackclubers.push(...result);
+  });
+})();
+
 
 (async () => {
   try {
@@ -11,29 +44,18 @@ import { loginToSlack } from './utils/login-to-slack';
     await page.waitForSelector('[data-qa="message_input"]');
 
     // send a message to the channel
-    await page.click('[data-qa="message_input"]');
-    await page.keyboard.type('/bag @kieran');
-    await page.keyboard.press('Enter');
-    await page.keyboard.press('Enter');
-    await page.waitForTimeout(500);
+    async function sendCommand(command: string) {
+      await page.click('[data-qa="message_input"]');
+      await page.keyboard.type(command);
+      await page.keyboard.press('Enter');
+      await page.keyboard.press('Enter');
+      await page.waitForTimeout(1000);
+    }
 
-    // wait for new message to load
-    await page.waitForSelector('.c-message_kit__blocks span');
-
-    await page.$$eval('.c-message_kit__blocks span', (spans, color) => {
-      for (let span of spans) {
-        span.style.border = `2px solid ${color}`;
-      }
-    }, 'red'); // 'red' is the border color, you can change it to any valid CSS color
-
-    // Extract the text content
-    const spansText = await page.$$eval('.c-message_kit__blocks span', spans => {
-      // get
-      return spans.map(span => span.textContent?.trim() || '').join(', ');
-    });
-
-    console.log(spansText);
-    await page.screenshot({ path: 'screenshot.png' });
+    for (let i = 0; i < hackclubers.length; i++) {
+      const { name } = hackclubers[i];
+      await sendCommand('/bag @' + name);
+    }
 
     await browser.close();
     console.log('Browser closed');

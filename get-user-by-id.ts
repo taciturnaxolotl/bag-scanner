acc06ce1df784ac8dc506a499da4f92f372a9f3f
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-02-21 00:14:52 -0500

    feat: add functionality to get user details from Slack API
    This commit adds the ability to fetch user details from the Slack API. The `getUser` function makes a request to the `users.info` endpoint, passing the `UserID` as a parameter. The request includes the `SLACK_OAUTH_TOKEN` in the `Authorization` header. The response is then parsed as JSON and returned.
    
    The code also includes a user prompt to enter the Slack UserID to search for. If a user is found, their details are displayed, including their name, display name, email, phone number, timezone, and admin status. If no user is found, a warning message is logged.
    
    The commit also includes introductory and outro prompts for a better user experience.

---
+++ get-user-by-id.ts
@@ -0,0 +1,46 @@
+import { intro, outro, text, note, log } from "@clack/prompts";
+
+async function getUser(UserID: string) {
+  const response = await fetch(
+    `https://slack.com/api/users.info?user=${UserID}&pretty=1`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.SLACK_OAUTH_TOKEN}`,
+      },
+    },
+  );
+
+  return response.json();
+}
+
+// ask user for the name of the user to get details for
+
+intro("Get User Details");
+
+const id = await text({
+  message: "Enter the slack UserID you would like to search for:",
+  placeholder: "Not sure",
+  initialValue: "",
+  validate(value) {
+    if (value.length === 0) return `Value is required!`;
+  },
+});
+
+// get the user details for the user with the given name
+// if the user is not found, return an error message
+const user = await getUser(id as string);
+
+if (user.ok === false) {
+  log.warn(`No user found with the id ${String(id)}.`);
+} else {
+  note(`  ${user.user.profile.pronouns ? `${user.user.profile.pronouns} ${user.user.real_name}` : user.user.real_name}
+  ---
+  @${user.user.profile.display_name_normalized}
+  email: ${user.user.profile.email ? user.user.profile.email : "No email provided"}
+  phone: ${user.user.profile.phone.length > 0 ? user.user.profile.phone : "No phone number provided"}
+  timezone: ${user.user.tz ? `${user.user.tz} - ${user.user.tz_label}` : "No timezone provided"}
+  ${user.user.deleted ? "user.user has been deleted :(" : `User is active & ${user.user.is_admin ? (user.user.is_owner ? "User is an Admin and a owner" : "User is an Admin") : "User is not an admin"}`}
+  ---`);
+}
+
+outro("Bye!");

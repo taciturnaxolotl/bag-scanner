e5d01adce3ab7166ad507e1ec3d264cf3856bb3e
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-03-07 22:12:28 -0500

    feat: add interactive functionality to search for emoji combinations
    The commit introduces an interactive feature that allows users to search for emoji combinations based on selected base emojis. Users can now select to search for another base emoji or search for emoji combinations. Additionally, the commit includes an option to exit the search process with a "Goodbye!" message displayed at the end of the search.

--- tools/add-emoji.ts
+++ tools/add-emoji.ts
@@ -117,6 +117,121 @@ async function searchBaseEmojis() {
       `Emoji found: ${emoji.emoji} (id: ${emoji.id})\nCategory: ${emoji.category}\nSubcategory: ${emoji.subcategory}\nKeywords: ${emoji.keywords.join(", ")}`,
     );
   }
+
+  if (emojis.length > 0) {
+    const nextAction = await select({
+      message: "What would you like to do next?",
+      options: [
+        {
+          value: "search-base",
+          label: "Search for another base emoji",
+        },
+        {
+          value: "search-combination",
+          label: "Search for emoji combinations",
+        },
+      ],
+    });
+
+    if (nextAction === "search-base") {
+      await searchBaseEmojis();
+    } else if (nextAction === "search-combination") {
+      await searchCombinations(emojis);
+    }
+  } else {
+    log.error("No emojis found");
+  }
+}
+
+async function searchCombinations(baseEmojis: emoji[]) {
+  let searchEmoji: string;
+
+  if (baseEmojis.length > 1) {
+    searchEmoji = (await select({
+      message: "Which emoji would you like to search for?",
+      options: baseEmojis.map((emoji) => ({
+        value: emoji.id,
+        label: emoji.emoji,
+      })),
+    })) as string;
+  } else {
+    log.info(
+      "Only one emoji found, searching for combinations with that emoji",
+    );
+    searchEmoji = baseEmojis[0].id;
+  }
+
+  const addingEmojis = spinner();
+
+  addingEmojis.start("Adding emojis to search vector");
+
+  const combinationEmojis = await create({
+    schema: {
+      name: "string",
+      leftEmoji: "string",
+      rightEmoji: "string",
+    },
+  });
+
+  for (const combinationEmoji of emojiKitchenData.data[searchEmoji]
+    .combinations) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    await insert(combinationEmojis, {
+      name: combinationEmoji.alt,
+      leftEmoji: combinationEmoji.leftEmoji,
+      rightEmoji: combinationEmoji.rightEmoji,
+    });
+  }
+
+  addingEmojis.stop("Emojis added to search vector");
+
+  const searchTerm = await text({
+    message: "What combination emoji would you like to search for?",
+    placeholder: "Not sure",
+    initialValue: "",
+    validate(value) {
+      if (value.length === 0) return `Value is required!`;
+    },
+  });
+
+  const searchResult = await search(combinationEmojis, {
+    term: searchTerm as string,
+  });
+
+  if (searchResult.hits.length !== 0) {
+    for (const hit of searchResult.hits) {
+      note(
+        `Combination Emoji found: ${hit.document.name}\nleft emoji: ${hit.document.leftEmoji}\nright emoji: ${hit.document.rightEmoji}`,
+      );
+    }
+  } else {
+    log.error("No emojis found");
+  }
+
+  const nextAction = await select({
+    message: "What would you like to do next?",
+    options: [
+      {
+        value: "search-combination",
+        label: "Search for another emoji combination",
+      },
+      {
+        value: "search-base",
+        label: "Search for another emoji base",
+      },
+      {
+        value: "exit",
+        label: "Exit",
+      },
+    ],
+  });
+
+  if (nextAction === "search-base") {
+    await searchBaseEmojis();
+  } else if (nextAction === "search-combination") {
+    await searchCombinations(baseEmojis);
+  }
 }
 
 try {
@@ -135,6 +250,8 @@ try {
   if (menu === "search") {
     await searchBaseEmojis();
   }
+
+  outro("Goodbye!");
 } catch (error) {
   log.error("Error");
   console.error(error);

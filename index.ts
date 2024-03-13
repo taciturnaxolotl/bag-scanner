9ba58e640ac84dd682c789ed156ff5bc6ce2fd5c
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-03-13 17:11:51 -0400

    feat: Add timeout for waiting new message and stop mining on SIGINT event

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import puppeteer from 'puppeteer';
 import { loginToSlack } from './utils/login-to-slack';
 
 async function waitForNewMessage(page: any) {
+  const start = Date.now();
+
   return new Promise(async (resolve) => {
     const message = await page.evaluate(() => {
       const messages = document.querySelectorAll('.p-rich_text_section');
@@ -20,6 +22,8 @@ async function waitForNewMessage(page: any) {
 
     if (message && ((message.includes('What you') || message.includes("nothing good here")) && !message.includes(':loading-dots:'))) {
       resolve(message);
+    } else if (start + 30000 < Date.now() {
+      resolve('timeout after 30 seconds of waiting for a new message.');
     } else {
       setTimeout(() => {
         resolve(waitForNewMessage(page));
@@ -44,9 +48,18 @@ async function waitForNewMessage(page: any) {
     }
 
     await sendCommand('Mining time!');
-    await new Promise(r => setTimeout(r, 1000));
+    await new Promise(r => setTimeout(r, 2000));
 
-    for (let i = 0; i < 100; i++) {
+    let i = 0;
+    let running = true;
+
+    // Listen for SIGINT event
+    process.on('SIGINT', () => {
+      console.log('Received SIGINT. Stopping...');
+      running = false;
+    });
+
+    while (true) {
       console.log('Sending command to use pickaxe #' + i);
       await sendCommand('/use pickaxe');
 
@@ -55,10 +68,12 @@ async function waitForNewMessage(page: any) {
       await waitForNewMessage(page).then((message: any) => {
         console.log('Received message: ' + message);
       });
+
+      i++;
     }
 
+    // Close the browser when done
     await browser.close();
-    console.log('Browser closed');
   } catch (error) {
     throw error
   }
